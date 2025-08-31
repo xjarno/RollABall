10c18c11d114eb14dd0b4fd3570a1401d0328ab2
Jarno3003 <jarnox3003@gmail.com>
2025-08-31 19:15:14 +0200

    First concept of clicking to move works

--- RollABall/Assets/Scripts/Player/PlayerMovement.cs
+++ RollABall/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,15 +2,53 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody rb;
+    private float hitForce;
+    private float holdDownStartTime;
+    private const float maxForce = 10f;
+   
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            holdDownStartTime = Time.time;
+            Debug.Log("Mouse");
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            float holdDownTime = Time.time - holdDownStartTime;
+            rb.AddForce(0, CalculateForce(holdDownTime), 0, ForceMode.Impulse);
+            Debug.Log(CalculateForce(holdDownTime));
+        }
+    }
+
+    private void Move()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            holdDownStartTime = Time.time;
+            Debug.Log("Mouse");
+        }
+
+        if (Input.GetMouseButtonUp(0)) 
+        { 
+            float holdDownTime = Time.time - holdDownStartTime;
+            rb.AddForce(0,CalculateForce(holdDownTime),0,ForceMode.Impulse);
+            Debug.Log(CalculateForce(holdDownTime));
+        }
+    }
+
+    private float CalculateForce(float holdTime) 
+    {
+        float maxForceHoldDownTime = 2f;
+        float holdTimeNormalized = Mathf.Clamp01(holdTime / maxForceHoldDownTime);
+        hitForce = holdTimeNormalized * maxForce;
+        return hitForce;
     }
 }
