53e52e8d05df0346f184b71dcb746be029fa1873
Jarno3003 <jarnox3003@gmail.com>
2025-08-31 20:24:23 +0200

    Added Golfball Texture and a cooldown until the ball is not moving

--- RollABall/Assets/Scripts/Player/PlayerMovement.cs
+++ RollABall/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,7 +8,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     private float hitForce;
     private float holdDownStartTime;
-    private const float maxForce = 10f;
+    private const float maxForce = 15f;
+    private bool isMoving = false;
     [SerializeField] Transform cam;
    
     void Start()
@@ -16,7 +19,10 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        if(rb.IsSleeping()) 
+        {
         Move();
+        }
     }
 
     private void Move()
@@ -32,18 +38,16 @@ public class PlayerMovement : MonoBehaviour
         { 
             //adds force to player in camera direction.
             float holdDownTime = Time.time - holdDownStartTime;
-            rb.AddForce(cam.forward.x * CalculateForce(holdDownTime),CalculateForce(holdDownTime), cam.forward.z * CalculateForce(holdDownTime), ForceMode.Impulse);
+            rb.AddForce(cam.forward.x * CalculateForce(holdDownTime), 0, cam.forward.z * CalculateForce(holdDownTime), ForceMode.Impulse);
             Debug.Log(CalculateForce(holdDownTime));
         }
     }
-
-    
 
 
     private float CalculateForce(float holdTime) 
     {
         // calculates how much force based on how long mousebutton is pressed.
-        float maxForceHoldDownTime = 2f;
+        float maxForceHoldDownTime = 1.5f;
         float holdTimeNormalized = Mathf.Clamp01(holdTime / maxForceHoldDownTime);
         hitForce = holdTimeNormalized * maxForce;
         return hitForce;
