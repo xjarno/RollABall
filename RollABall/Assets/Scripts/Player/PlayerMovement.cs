db0ee6ab62195914c58722ef0fea30b8372202ab
Jarno3003 <jarnox3003@gmail.com>
2025-08-31 19:32:06 +0200

    Added movement to the player

--- RollABall/Assets/Scripts/Player/PlayerMovement.cs
+++ RollABall/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Variables
     private Rigidbody rb;
     private float hitForce;
     private float holdDownStartTime;
     private const float maxForce = 10f;
+    [SerializeField] Transform cam;
    
     void Start()
     {
@@ -14,41 +16,37 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            holdDownStartTime = Time.time;
-            Debug.Log("Mouse");
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            float holdDownTime = Time.time - holdDownStartTime;
-            rb.AddForce(0, CalculateForce(holdDownTime), 0, ForceMode.Impulse);
-            Debug.Log(CalculateForce(holdDownTime));
-        }
+        Move();
     }
 
     private void Move()
     {
         if (Input.GetMouseButtonDown(0)) 
         {
+            //starttime of when mousebutton is pressed
             holdDownStartTime = Time.time;
             Debug.Log("Mouse");
         }
 
         if (Input.GetMouseButtonUp(0)) 
         { 
+            //adds force to player in camera direction.
             float holdDownTime = Time.time - holdDownStartTime;
-            rb.AddForce(0,CalculateForce(holdDownTime),0,ForceMode.Impulse);
+            rb.AddForce(cam.forward.x * CalculateForce(holdDownTime),CalculateForce(holdDownTime), cam.forward.z * CalculateForce(holdDownTime), ForceMode.Impulse);
             Debug.Log(CalculateForce(holdDownTime));
         }
     }
 
+    
+
+
     private float CalculateForce(float holdTime) 
     {
+        // calculates how much force based on how long mousebutton is pressed.
         float maxForceHoldDownTime = 2f;
         float holdTimeNormalized = Mathf.Clamp01(holdTime / maxForceHoldDownTime);
         hitForce = holdTimeNormalized * maxForce;
         return hitForce;
     }
+
 }
